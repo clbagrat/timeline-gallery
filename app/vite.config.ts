0dd274fa93f58a358d7318aaa555d2a7bfc977c6
clbagrat <gobedashvilibagrat@gmail.com>
2022-03-21 23:16:25 +0100

    init

---
+++ app/vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import path from 'path';
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/main.ts'),
+      name: 'app',
+      fileName: (format) => `app.${format}.js`
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['react'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: 'React',
+        },
+      },
+    },
+  },
+  plugins: [react()]
+})
